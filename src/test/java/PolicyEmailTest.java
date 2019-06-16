16d9549191407f4c2b7e868a16ffc503c94d9768
Prashant Patel <12061@sbigeneral.in>
2019-06-16 12:52:46 +0530

    organised imports

--- src/test/java/PolicyEmailTest.java
+++ src/test/java/PolicyEmailTest.java
@@ -3,7 +3,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,7 +12,6 @@ import io.kubesure.comms.PolicyIssuedEvent;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = EmailSubscriber.class)
 public class PolicyEmailTest {
-    //@Autowired EmailSubscriber sub; 
     @Test
     public void testPolicyIssuedEmail() throws JsonProcessingException {
         EmailSubscriber sub = new EmailSubscriber();
